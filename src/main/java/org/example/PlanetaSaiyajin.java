e155324c3893988ba96edb2b211306da2cca7b84
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-03-09 10:38:17 -0500

    v1

---
+++ src/main/java/org/example/PlanetaSaiyajin.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class PlanetaSaiyajin {
+    public static void main(String[] args) {
+
+
+        ClaseAlta guerrero1 = new ClaseAlta("Vegetta", 90, 30, "Negro");
+        ClaseBaja guerrero2 = new ClaseBaja("Goku", 80, 25, "Negro");
+
+        guerrero1.pelear();
+        guerrero2.pelear();
+    }
+}
---
+++ src/main/java/org/example/Sayayin.java
@@ -0,0 +1,60 @@
+package org.example;
+
+// Se declara la clase como abstract
+public abstract class Sayayin {
+    private String nombre;
+    private Integer poderPelea;
+    private Integer edad;
+    private String colorPelo;
+
+    public Sayayin() {
+    }
+
+    public Sayayin(String nombre, Integer poderPelea, Integer edad, String colorPelo) {
+        this.nombre = nombre;
+        this.poderPelea = poderPelea;
+        this.edad = edad;
+        this.colorPelo = colorPelo;
+    }
+
+    public void comer() {
+        System.out.println("Comiendo");
+    }
+
+    // Metodo abstracto, solo declara el metodo pelear, no define el funcionamieto
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getPoderPelea() {
+        return poderPelea;
+    }
+
+    public void setPoderPelea(Integer poderPelea) {
+        this.poderPelea = poderPelea;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getColorPelo() {
+        return colorPelo;
+    }
+
+    public void setColorPelo(String colorPelo) {
+        this.colorPelo = colorPelo;
+    }
+
+    public abstract void pelear();
+
+}
