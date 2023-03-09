d28a649389081a1bf359a5ffcdfa4d98c1ef04dd
Manuel Lopera <114296815+LoperaUr@users.noreply.github.com>
2023-03-09 10:39:43 -0500

    delete sout

--- src/main/java/org/example/PlanetaSaiyajin.java
+++ src/main/java/org/example/PlanetaSaiyajin.java
@@ -3,7 +3,6 @@ package org.example;
 public class PlanetaSaiyajin {
     public static void main(String[] args) {
 
-
         ClaseAlta guerrero1 = new ClaseAlta("Vegetta", 90, 30, "Negro");
         ClaseBaja guerrero2 = new ClaseBaja("Goku", 80, 25, "Negro");
 
